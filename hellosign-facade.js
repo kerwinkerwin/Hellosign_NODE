2a3afb30203257c8734cd3df2190bf1b279480bd
kerwinkerwin <kerwin@enspiral.com>
2015-08-17 15:48:43 +1200

    T&C template sending correctly, set to test mode in options

--- hellosign-facade.js
+++ hellosign-facade.js
@@ -7,14 +7,13 @@ var auth = {
     pass:"",
     sendImmediately:true
     }
-
+var hellosign = require('hellosign-sdk')({key:process.env.HELLOSIGN_KEY});
 var headers ={
     'Accept':'application/json',
     'Content-Type':'application/json'
   };
 
 var getTemplateList = function getTemplateList(callback){
-  console.log("why?");
   unirest.get(baseUri + '/template/list')
     .auth(auth)
     .header(headers)
@@ -23,6 +22,39 @@ var getTemplateList = function getTemplateList(callback){
     })
 };
 
+var signTemplate = function signTemplate(student, callback){
+  console.log("hello?")
+  var options = {
+    test_mode:1,
+    template_id: 'aa67d4e8143421720fba63b326656e63aff924eb',
+    subject: 'Dev Academy Terms and Conditions',
+    message: 'Hi ' + student.name + 'Please sign where required',
+    signers:[
+      {
+        email_address: student.email,
+        name: student.name,
+        role: 'Student'
+      }
+    ],
+    custom_fields: {
+      "Student Name": student.name
+    }
+  };
+  console.log(options);
+
+  hellosign.signatureRequest.sendWithTemplate(options)
+    .then(function(response){
+      console.log("this");
+      console.log(response);
+    })
+    .catch(function(err){
+      console.log("2");
+      console.log(err);
+    });
+
+};
+
 module.exports ={
-  getTemplateList:getTemplateList
+  getTemplateList:getTemplateList,
+  signTemplate: signTemplate
 };
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/list', function(req,res,next){
+router.get('/templates', function(req,res,next){
   hellosign.getTemplateList(function(response){
     res.status(200).json(response)
   });
 });
+
+router.post('/templates/sign', function(req,res,next){
+  // console.log(req.body);
+  var student = req.body;
+  hellosign.signTemplate(student,function(response){
+    console.log(response);
+  })
+})
 
 module.exports = router;
